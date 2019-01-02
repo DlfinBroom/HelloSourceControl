b465909caf13918b73afa8a9d640bcb3e4b4e521
lampert309 <lampert309@gmail.com>
2019-01-02 14:56:52 -0800

    Made some changes as a test

--- HelloSourceControl/Form1.Designer.cs
+++ HelloSourceControl/Form1.Designer.cs
@@ -23,13 +23,46 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.tbxHelloWorld = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxHelloWorld
+            // 
+            this.tbxHelloWorld.Enabled = false;
+            this.tbxHelloWorld.Location = new System.Drawing.Point(12, 14);
+            this.tbxHelloWorld.Name = "tbxHelloWorld";
+            this.tbxHelloWorld.Size = new System.Drawing.Size(75, 20);
+            this.tbxHelloWorld.TabIndex = 0;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(93, 11);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 25);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Exit";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(261, 72);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.tbxHelloWorld);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbxHelloWorld;
+        private System.Windows.Forms.Button btnClose;
     }
 }
 
--- HelloSourceControl/Form1.cs
+++ HelloSourceControl/Form1.cs
@@ -12,6 +12,12 @@ namespace HelloSourceControl {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+
+            tbxHelloWorld.Text = "Hello World";
+        }
+
+        private void btnClose_Click(object sender, EventArgs e) {
+            Close();
         }
     }
 }
